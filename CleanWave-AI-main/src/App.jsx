c20a085b0883d602fa5781fde0a3a8b856dd5692
rudrapatel28 <94849213+rudrapatel28@users.noreply.github.com>
2023-11-05 10:57:42 -0500

    Add files via upload

---
+++ CleanWave-AI-main/src/App.jsx
@@ -0,0 +1,101 @@
+						
+import { useState } from "react";
+import "./App.css";
+import { Configuration, OpenAIApi } from "openai";
+
+
+const configuration = new Configuration({
+  organization: "org-i9bmP1P9lMzVcQ3wpdVGD292",	
+  apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
+
+function App() {
+  const [message, setMessage] = useState("");
+  const [chats, setChats] = useState([]);
+  const [isTyping, setIsTyping] = useState(false);
+
+
+  const chat = async (e, message) => {
+    e.preventDefault();
+ 
+    if (!message) return;
+    setIsTyping(true);
+    scrollTo(0, 1e10);
+ 
+    let msgs = chats;
+    msgs.push({ role: "user", content: message });
+    setChats(msgs);
+ 
+    setMessage("");
+ 
+    await openai
+      .createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "system",
+            content:
+              "You are a Dr.Clean. When someone asks what your name is you can Dr.Clean. You will ask the user 3 trivia questions related to the ocean, one by one. Ask new questions everytime the user wants to try again",
+          },
+          ...chats,
+        ],
+      })
+      .then((res) => {
+        msgs.push(res.data.choices[0].message);
+        setChats(msgs);
+        setIsTyping(false);
+        scrollTo(0, 1e10);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+
+  return (
+    <main>
+      <h1>Dr.Clean's Trivia!</h1>
+
+
+      <section>
+        {chats && chats.length
+          ? chats.map((chat, index) => (
+              <p key={index} className={chat.role === "user" ? "user_msg" : ""}>
+                <span>
+                  <b>{chat.role.toUpperCase()}</b>
+                </span>
+                <span>:</span>
+                <span>{chat.content}</span>
+              </p>
+            ))
+          : ""}
+      </section>
+
+
+      <div className={isTyping ? "" : "hide"}>
+        <p>
+          <i>{isTyping ? "Typing" : ""}</i>
+        </p>
+      </div>
+
+
+      <form action="" onSubmit={(e) => chat(e, message)}>
+        <input
+          type="text"
+          name="message"
+          value={message}
+          placeholder="Type a message here and hit Enter..."
+          onChange={(e) => setMessage(e.target.value)}
+        />
+      </form>
+    </main>
+  );
+}
+
+
+export default App;
+
+
+
